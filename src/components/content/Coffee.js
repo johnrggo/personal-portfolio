709a9abfa435c0139c7517a52599db67db10fb5b
John Go <johnrggo@gmail.com>
2021-04-29 21:14:39 +0800

    w

--- src/components/content/Coffee.js
+++ src/components/content/Coffee.js
@@ -1,24 +1,34 @@
 import React from "react";
 import styled from "styled-components";
 import { TextDecrypt } from "../content/TextDecrypt";
-import { Typography } from "@material-ui/core";
+import { Tooltip, Typography, Zoom } from "@material-ui/core";
+
+const Container = styled.div`
+  width: 300px;
+
+  /* @media (max-width: 500px) {
+    display: none;
+  } */
+`;
 
 const Button = styled.a`
   line-height: 36px;
-  height: 3rem;
+  height: 3.75rem;
+  /* min-width: 150px; */
   padding-right: 1rem;
   text-decoration: none;
   display: flex;
   color: #ffffff;
   background-color: #40dca5;
-  border-radius: 3px;
+  border-radius: 6px;
   border: 1px solid transparent;
   padding: 1px 9px;
+  padding-left: 1rem;
   font-size: 23px;
   letter-spacing: 0.6px;
   box-shadow: 0px 1px 2px rgba(190, 190, 190, 0.5);
   -webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5);
-  margin: 0 auto 3.5rem 2rem;
+  margin: 0 auto 3rem 3rem;
   font-family: "Cookie", cursive;
   -webkit-box-sizing: border-box;
   box-sizing: border-box;
@@ -37,10 +47,27 @@ const Button = styled.a`
     opacity: 0.85;
     color: black;
   }
+
+  @media (max-width: 500px) {
+    width: 60px;
+  }
+
+  > .button__text {
+    padding-top: 4px;
+    padding-left: 4px;
+
+    @media (max-width: 500px) {
+      display: none;
+    }
+
+    @media (max-width: 600px) {
+      font-size: 1.1667rem;
+    }
+  }
 `;
 
 const Image = styled.img`
-  width: 27px;
+  width: 26px;
   margin-right: 4px;
   box-shadow: none;
   border: none;
@@ -49,16 +76,24 @@ const Image = styled.img`
 
 function Coffee() {
   return (
-    <Button target="_blank" href="https://www.buymeacoffee.com/johngo">
-      <Image
-        src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
-        onerror="this.onerror=null;this.src='https://user-images.githubusercontent.com/16066404/77041853-a2044100-69e0-11ea-8da6-d64822a2c72a.jpg'"
-        alt="Buy me a coffee"
-      />
-      <Typography variant="body1">
-        <TextDecrypt text={" Buy me a coffee"} />
-      </Typography>
-    </Button>
+    <Container>
+      <Tooltip
+        title={"Coffee plith 🥺👉🏽👈🏽"}
+        placement="right"
+        TransitionComponent={Zoom}
+      >
+        <Button target="_blank" href="https://www.buymeacoffee.com/johngo">
+          <Image
+            src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
+            onerror="this.onerror=null;this.src='https://user-images.githubusercontent.com/16066404/77041853-a2044100-69e0-11ea-8da6-d64822a2c72a.jpg'"
+            alt="Buy me a coffee"
+          />
+          <Typography className="button__text" variant="body1">
+            <TextDecrypt text={" Buy me a coffee"} />
+          </Typography>
+        </Button>
+      </Tooltip>
+    </Container>
   );
 }
 
